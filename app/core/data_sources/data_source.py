7331747667693c2f8618fb650bf4bc8b021a68b9
ofir hodara <ofir1682002@gmail.com>
2023-09-26 00:51:41 +0300

    Wrote the base architecture for start parse data

--- app/core/data_sources/data_source.py
+++ app/core/data_sources/data_source.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from pydantic.main import BaseModel
+
+from app.core.lab.laboratory import LabDataItem
 from app.core.serializer_methods.serializer import IBaseSerializer
 from app.exceptions.lab_exceptions import SerializationError
 from app.managers.logger_manager import create_logger
@@ -9,15 +12,53 @@ logger = create_logger(__name__)
 
 
 class IDataLabService(ABC):
-    _data: Any
-    _serializer: IBaseSerializer
-    _lab_object:
+    """Abstract base class for data lab services.
+
+    This abstract class defines the common interface for data lab services. Subclasses should implement the
+    `read_data` method to read and process data.
+
+    Attributes:
+        _data (Any): The processed data.
+        _serializer (IBaseSerializer): The serializer to use for parsing data.
+        _lab_object (BaseModel): The Pydantic model representing the lab object.
+
+    Methods:
+        read_data: Abstract method to read and process data.
+        _process_data_format: Internal method to process data using the serializer.
+        get_data: Property to access the processed data.
+
+    """
+
+    _data: Any = None
+    _serializer: IBaseSerializer = ...
+    _lab_target_class: LabDataItem = ...
+
+    def __init__(self, *, serializer, target_class):
+        self._serializer = serializer
+        self._lab_target_class = target_class
 
     @abstractmethod
     def read_data(self, *args, **kwargs) -> Any:
+        """Read and process data from a data source.
+
+        Subclasses must implement this method to read data from a specific data source and process it.
+
+        Returns:
+            Any: The processed data.
+        """
         pass
 
     def _process_data_format(self, raw_data):
+        """Process data using the specified serializer.
+
+        This method uses the serializer to parse and process raw data.
+
+        Args:
+            raw_data: The raw data to be processed.
+
+        Raises:
+            SerializationError: If there is an error during serialization.
+        """
         try:
             self._data = self._serializer.parse(raw_data)
         except SerializationError as e:
@@ -25,4 +66,22 @@ class IDataLabService(ABC):
 
     @property
     def get_data(self):
+        """Property to access the processed data.
+
+        Returns:
+            Any: The processed data.
+        """
         return self._data
+
+    def parse_data(self):
+        # read the data
+        pass
+
+        # process the data
+        pass
+
+        # build parser with factory
+        pass
+
+        # parse the data
+        pass
--- app/core/data_sources/file_source.py
+++ app/core/data_sources/file_source.py
@@ -4,6 +4,7 @@ from typing import Any
 from fastapi import UploadFile
 
 from app.core.data_sources.data_source import IDataLabService
+from app.core.lab.laboratory import LabDataItem
 from app.core.serializer_methods.serializer import IBaseSerializer
 from app.managers.logger_manager import create_logger
 
@@ -11,26 +12,65 @@ logger = create_logger(__name__)
 
 
 class FileSource(IDataLabService):
-    def __init__(self, file: UploadFile, _serializer: IBaseSerializer):
+    """Represents a data source for reading data from an uploaded file.
+
+    This class is designed to read data from an uploaded file and process it using a specified serializer.
+
+    Attributes:
+        file (UploadFile): The uploaded file to read data from.
+        _serializer (IBaseSerializer): The serializer to use for parsing data.
+
+    Methods:
+        read_data: Read and process data from the uploaded file.
+
+    """
+
+    def __init__(self, file: UploadFile, serializer: IBaseSerializer, lab_class: LabDataItem):
+        super().__init__(serializer=serializer, target_class=lab_class)
         self.file = file
-        self._serializer = _serializer
+        self._serializer = serializer
 
     def read_data(self, *args, **kwargs) -> Any:
-        # handle the data into string from files
-        raw_data = self.file.read()
-        super()._process_data_format(self.file)
+        """Read and process data from the uploaded file.
+
+        This method reads data from the uploaded file, processes it using the specified serializer,
+        and returns the processed data.
+
+        Returns:
+            Any: The processed data.
+        """
+        # Read the data from the file into a string
+        self._data = self.file.read()
 
 
 class ReadFileSource(IDataLabService):
-    """
-    Consider we want the server to read the data from file path
-    i will send him as request the file path to read from and it will do it for me
-    so we have one classic FileSource and the second one will be for future reading
+    """Represents a data source for future reading from a file path.
+
+    This class is designed for scenarios where data will be read from a file path in the future.
+    It serves as a placeholder for such operations. Important to understand it!
+
+    Attributes:
+        file (Path): The file path to read data from.
+        _serializer (IBaseSerializer): The serializer to use for parsing data.
+
+    Methods:
+        read_data: This method is not implemented and raises a NotImplementedError.
+
     """
 
-    def __init__(self, file_path: Path, _serializer: IBaseSerializer):
-        self.file = file_path
-        self._serializer = _serializer
+    def __init__(self, *,
+                 file_path: Path,
+                 serializer: IBaseSerializer,
+                 lab_class: LabDataItem, target_class):
+        super().__init__(serializer=serializer, target_class=target_class)
+        self.file_path = file_path
 
     def read_data(self, *args, **kwargs) -> Any:
+        """Read and process data from the file path.
+
+        This method is not implemented and raises a NotImplementedError.
+
+        Raises:
+            NotImplementedError: This method is not implemented in this class.
+        """
         raise NotImplementedError()
