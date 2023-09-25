7c0cbd22d948097549984f945f628887da4d9fd2
ofir hodara <ofir1682002@gmail.com>
2023-09-26 00:52:07 +0300

    Make Data lab Base class inherit from BadeModel

--- app/core/lab/items.py
+++ app/core/lab/items.py
@@ -1,24 +1,24 @@
 import uuid
+
 from app.core.lab.items_details import ItemConnection, ItemState, ItemQuality
-
-from pydantic import BaseModel
-
-
-class Card(BaseModel):
-    component_id: uuid.UUID
-    index: int
-    card_type: ItemQuality
+from app.core.lab.laboratory import LabDataItem
 
 
-class Port(BaseModel):
-    component_id: uuid.UUID
-    state: ItemConnection  # is connected?
-    index: int
-    card_index: int
+class Card(LabDataItem):
+    component_id: uuid.UUID = ...
+    index: int = ...
+    card_type: ItemQuality = ...
 
 
-class Component(BaseModel):
-    comp_id: uuid.UUID
-    version: int
-    state: ItemState
-    card_index: int
+class Port(LabDataItem):
+    component_id: uuid.UUID = ...
+    state: ItemConnection = ...
+    index: int = ...
+    card_index: int = ...
+
+
+class Component(LabDataItem):
+    comp_id: uuid.UUID = ...
+    version: int = ...
+    state: ItemState = ...
+    card_index: int = ...
--- app/core/lab/laboratory.py
+++ app/core/lab/laboratory.py
@@ -1,15 +1,34 @@
 from typing import List, Dict, Generic
-from pydantic import BaseModel
 
-from app.core.lab.items import Port, Card, Component
+from pydantic import BaseModel
 from result import Ok, Err
 
+from app.core.lab.items import Port, Card, Component
 from app.managers.logger_manager import create_logger
 
 logger = create_logger(__name__)
 
 
-class Laboratory(BaseModel):
+class LabDataItem(BaseModel):
+    pass
+
+
+class Laboratory(LabDataItem):
+    """Represents a laboratory with components, cards, and ports.
+
+    This Pydantic model represents a laboratory with its various components, cards, and ports.
+
+    Attributes:
+        _laboratory_class_mapper (Dict[str, BaseModel]): A mapping of class names to their corresponding Pydantic models.
+        component (List[Component]): A list of Component instances.
+        cards (List[Card]): A list of Card instances.
+        ports (List[Port]): A list of Port instances.
+
+    Methods:
+        get_lab_class_by_name: Retrieve a class type based on its name.
+
+    """
+
     _laboratory_class_mapper: Dict[str, BaseModel] = {
         "Component": Component,
         "Card": Card,
@@ -21,6 +40,18 @@ class Laboratory(BaseModel):
 
     @staticmethod
     def get_lab_class_by_name(class_name: str) -> Generic:
+        """Retrieve a class type based on its name.
+
+        This static method allows you to retrieve a class type (Pydantic model) based on its name.
+        It is useful for dynamically selecting a model based on input data.
+
+        Args:
+            class_name (str): The name of the class to retrieve.
+
+        Returns:
+            Generic: An instance of the class if found, or an error result if not found.
+
+        """
         if class_name in Laboratory._laboratory_class_mapper.keys():
             return Ok(Laboratory._laboratory_class_mapper[class_name])
         e_message = f"The class {class_name} is not one of our Laboratory items classes"
