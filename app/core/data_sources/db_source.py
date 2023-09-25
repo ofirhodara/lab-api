d5069ab46d95c43b45f3b5b6ae4faea1469b95af
ofir hodara <ofir1682002@gmail.com>
2023-09-25 23:06:34 +0300

    implement db data source for example

--- app/core/data_sources/db_source.py
+++ app/core/data_sources/db_source.py
@@ -2,13 +2,22 @@ from app.core.data_sources.data_source import DataSource
 
 
 class DbSource(DataSource):
-    def __init__(self, host, port, table, sql_query):
+    def __init__(self, host, port, connection_str, *args, **kwargs):
         self.host = host
         self.port = port
-        self.table = table
-        self.sql_query = sql_query
+        self.connection_str = connection_str
+
+    @abstract_method
+    def read_data(self, *args, **kwargs):
+        raise NotImplementedError("Non implemented yet!")
+
+
+class ElasticSource(DbSource):
+    def __init__(self, index, host, port, connection_str, *args, **kwargs):
+        super().__init__(host, port, connection_str, *args, **kwargs)
+        self.index = index
 
     def read_data(self, *args, **kwargs):
-        pass
+        raise NotImplementedError("Non implemented yet!")
 
 # TODO: Redis, Elastic.....
